a919581ca9db44f38ad6386e66dc2a20749de9d8
kate hollenbach <kjhollen@gmail.com>
2017-07-26 12:29:12 -0700

    second set of changes for shader refactor.
    - removes material hash from RendererGL in favor of methods that
      cache and reference specific GL shaders.
    - updates code that used to depend on material hash to more specifically
      set a particular type of shader at render time.
    - adds some (not yet right) inline documentation tags and unit tests.
    
    still todo:
    - figure out why unit tests don't run
    - move texture handling into shader
    - add unit tests for all shaders and setShader method
    - fix documentation structure
    - standardize prepping vertex arrays across immediate mode and retain
      mode (by moving to shader)
    - figure out if immediate and retain mode really need separate shaders

--- src/webgl/p5.RendererGL.js
+++ src/webgl/p5.RendererGL.js
@@ -4,7 +4,39 @@ var p5 = require('../core/core');
 require('./shader');
 require('../core/p5.Renderer');
 require('./p5.Matrix');
+var fs = require('fs');
+
 var uMVMatrixStack = [];
+
+//@TODO should implement public method
+//to override these attributes
+var attributes = {
+  alpha: true,
+  depth: true,
+  stencil: true,
+  antialias: false,
+  premultipliedAlpha: false,
+  preserveDrawingBuffer: false
+};
+
+var defaultShaders = {
+  immediateVert:
+    fs.readFileSync(__dirname + '/shaders/immediate.vert', 'utf-8'),
+  vertexColorVert:
+    fs.readFileSync(__dirname + '/shaders/vertexColor.vert', 'utf-8'),
+  vertexColorFrag:
+    fs.readFileSync(__dirname + '/shaders/vertexColor.frag', 'utf-8'),
+  normalVert:
+    fs.readFileSync(__dirname + '/shaders/normal.vert', 'utf-8'),
+  normalFrag:
+    fs.readFileSync(__dirname + '/shaders/normal.frag', 'utf-8'),
+  basicFrag:
+    fs.readFileSync(__dirname + '/shaders/basic.frag', 'utf-8'),
+  lightVert:
+    fs.readFileSync(__dirname + '/shaders/light.vert', 'utf-8'),
+  lightTextureFrag:
+    fs.readFileSync(__dirname + '/shaders/light_texture.frag', 'utf-8'),
+};
 
 /**
  * 3D graphics class
@@ -49,18 +81,27 @@ p5.RendererGL = function(elt, pInst, isMainCanvas, attr) {
   this.uNMatrix = new p5.Matrix('mat3');
   //Geometry & Material hashes
   this.gHash = {};
-  this.mHash = {};
+
+  this.emptyTexture = null;
+  this.curShader = null;
+
+  this._defaultLightShader = undefined;
+  this._defaultImmediateModeShader = undefined;
+  this._defaultNormalShader = undefined;
+  this._defaultColorShader = undefined;
+
+  this.setShader(this._getColorShader());
+
   //Imediate Mode
   //default drawing is done in Retained Mode
   this.isImmediateDrawing = false;
   this.immediateMode = {};
+  // note: must call fill() and stroke () AFTER
+  // default shader has been set.
   this.fill(255, 255, 255, 255);
   this.stroke(0, 0, 0, 255);
   this.pointSize = 5.0;//default point/stroke
   this.name = 'p5.RendererGL';   // for friendly debugger system
-  this.emptyTexture = null;
-
-  this.curShader = null;
 
   return this;
 };
@@ -255,125 +296,6 @@ p5.RendererGL.prototype.background = function() {
 // };
 
 //////////////////////////////////////////////
-// SHADER
-//////////////////////////////////////////////
-
-/**
- * [_initShaders description]
- * @param  {string} vertId [description]
- * @param  {string} fragId [description]
- * @return {Object} the shader program
- */
-p5.RendererGL.prototype._initShaders = function(vertId, fragId, isImmediateMode) {
-  var shaderProgram = new p5.Shader(this, vertId, fragId);
-  shaderProgram.init(); // make ready to use
-
-  //END SHADERS SETUP
-  this._createEmptyTexture();
-
-  // Inspect shader and cache uniform info
-
-  shaderProgram.uniforms = {};
-
-  var numUniforms = gl.getProgramParameter(shaderProgram,
-    gl.ACTIVE_UNIFORMS);
-
-  var samplerIndex = 0;
-  for(i = 0; i < numUniforms; ++i){
-    var uniformInfo = gl.getActiveUniform(shaderProgram, i);
-    var uniform = {};
-    uniform.location = gl.getUniformLocation(shaderProgram, uniformInfo.name);
-    uniform.size = uniformInfo.size;
-    var uniformName = uniformInfo.name;
-    //uniforms thats are arrays have their name returned as
-    //someUniform[0] which is a bit silly so we trim it
-    //off here. The size property tells us that its an array
-    //so we dont lose any information by doing this
-    if(uniformInfo.size > 1) {
-      uniformName = uniformName.substring(0,
-        uniformName.indexOf('[0]'));
-    }
-    uniform.name = uniformName;
-    uniform.type = uniformInfo.type;
-    if(uniform.type === gl.SAMPLER_2D)
-    {
-      uniform.samplerIndex = samplerIndex;
-      samplerIndex++;
-    }
-    shaderProgram.uniforms[uniformName] = uniform;
-  }
-
-  //END SHADERS SETUP
-
-  //This happens automatically in the above step
-  //this._getLocation(shaderProgram, isImmediateMode);
-
-  return shaderProgram;
-};
-
-
-/**
- * Wrapper around gl.useProgram to make sure
- * we only switch shaders when neccessary
- */
-p5.RendererGL.prototype._useShader = function(shaderProgram){
-  if (shaderProgram === this.curShader) {
-    return;
-  }
-  //shaderProgram.bind(); // TODO ? is this right?
-  this.curShader = shaderProgram;
-  this.curShader.useProgram();
-  return shaderProgram;
-};
-
-
-//////////////////////////////////////////////
-// GET CURRENT | for shader and color
-//////////////////////////////////////////////
-p5.RendererGL.prototype._getShader = function(vertId, fragId, isImmediateMode) {
-  var mId = vertId + '|' + fragId;
-
-  if(!this.materialInHash(mId)){
-    this._initShaders(vertId, fragId, isImmediateMode);
-    this.newShader = true;
-  }
-  this.curShaderId = mId;
-
-  return this.mHash[this.curShaderId];
-};
-
-p5.RendererGL.prototype._getCurShaderId = function(){
-  //if the shader ID is not yet defined
-  if (this.drawMode !== 'fill' && this.curShaderId === undefined){
-    //default shader: normalMaterial()
-    this._getShader('normalVert', 'normalFrag');
-  } else if (this.drawMode === 'fill'){
-    // note that this._getShader will check if the shader already exists
-    // by looking up the shader id (composed of vertexShaderId|fragmentShaderId)
-    // in the material hash. If the material isn't found in the hash, it
-    // creates a new one using this._initShaders--however, we'd like
-    // use the cached version as often as possible, so we defer to this._getShader
-    // here instead of calling this._initShaders directly.
-    var newShader = this.curShader;
-    if (this.isImmediateDrawing) {
-      newShader = this._getShader('immediateVert', 'vertexColorFrag', true);
-    } else {
-      // we're not in immediate mode drawing,
-      // but our shader is in the immediate mode fill.
-      // need to change it to basic shading.
-      if (this.curShaderId === 'immediateVert|vertexColorFrag' ||
-        this.curShaderId === undefined) {
-        newShader = this._getShader('normalVert', 'basicFrag', false);
-      }
-    }
-    // this should be safe, but...
-    this._useShader(newShader);
-  }
-
-  return this.curShaderId;
-};
-
-//////////////////////////////////////////////
 // COLOR
 //////////////////////////////////////////////
 /**
@@ -409,29 +331,22 @@ p5.RendererGL.prototype._getCurShaderId = function(){
  *
  */
 p5.RendererGL.prototype.fill = function(v1, v2, v3, a) {
-  var shaderProgram;
   //see material.js for more info on color blending in webgl
   var colors = this._applyColorBlend.apply(this, arguments);
   this.curFillColor = colors;
   this.drawMode = 'fill';
   if (this.isImmediateDrawing){
-    shaderProgram = this._getShader('immediateVert','vertexColorFrag');
-    this._useShader(shaderProgram);
+    this.setShader(this._getImmediateModeShader());
   } else {
-    shaderProgram = this._getShader('normalVert', 'basicFrag');
-    this._useShader(shaderProgram);
-    //RetainedMode uses a webgl uniform to pass color vals
-    //in ImmediateMode, we want access to each vertex so therefore
-    //we cannot use a uniform.
-    shaderProgram.setUniform('uMaterialColor', colors);
+    var shader = this.setShader(this._getColorShader());
+    shader.setUniform('uMaterialColor', colors);
   }
   return this;
 };
 
 p5.RendererGL.prototype.noFill = function() {
   var gl = this.GL;
-  var shaderProgram = this._getShader('normalVert', 'basicFrag');
-  this._useShader(shaderProgram);
+  this.setShader(this._getColorShader());
   gl.enable(gl.BLEND);
   gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
   this.drawMode = 'wireframe';
@@ -452,8 +367,8 @@ p5.RendererGL.prototype.stroke = function(r, g, b, a) {
 };
 
 p5.RendererGL.prototype._setNoFillStroke = function() {
-  var shaderProgram = this.mHash[this.curShaderId];
-  this._useShader(shaderProgram);
+  // this should only be called after an appropriate call
+  // to shader() internally....
   this.curShader.setUniform('uMaterialColor', this.curStrokeColor);
 };
 
@@ -533,17 +448,18 @@ p5.RendererGL.prototype.strokeWeight = function(pointSize) {
   this.pointSize = pointSize;
   return this;
 };
+
+
+
 //////////////////////////////////////////////
-// HASH | for material and geometry
+// HASH | for geometry
 //////////////////////////////////////////////
 
 p5.RendererGL.prototype.geometryInHash = function(gId){
   return this.gHash[gId] !== undefined;
 };
 
-p5.RendererGL.prototype.materialInHash = function(mId){
-  return this.mHash[mId] !== undefined;
-};
+
 
 /**
  * [resize description]
@@ -653,4 +569,78 @@ p5.RendererGL.prototype._applyTextProperties = function() {
   //@TODO finish implementation
   console.error('text commands not yet implemented in webgl');
 };
+
+
+
+//////////////////////////////////////////////
+// SHADER
+//////////////////////////////////////////////
+
+/*
+ * Initializes and uses the specified shader, then returns
+ * that shader. Note: initialization and resetting the program
+ * is only used if needed (say, if a new value is provided)
+ * so it is safe to call this method with the same shader multiple
+ * times without a signficant performance hit).
+ *
+ * @method setShader
+ * @param {p5.Shader} s a p5.Shader object
+ * @return {p5.Shader} the current, updated shader
+ */
+p5.RendererGL.prototype.setShader = function (s) {
+  if (this.curShader !== s) {
+    // only do setup etc. if shader is actually new.
+    this.curShader = s;
+
+    // safe to do this multiple times;
+    // init() will bail early if has already been run.
+    this.curShader.init();
+    this.curShader._useProgram();
+  }
+
+  // always return this.curShader, even if no change was made.
+  return this.curShader;
+};
+
+/*
+ * shaders are created and cached on a per-renderer basis,
+ * on the grounds that each renderer will have its own gl context
+ * and the shader must be valid in that context.
+ */
+
+p5.RendererGL.prototype._getLightShader = function () {
+  if (this._defaultLightShader === undefined) {
+    this._defaultLightShader = new p5.Shader(this,
+      defaultShaders.lightVert, defaultShaders.lightTextureFrag);
+  }
+  return this._defaultLightShader;
+};
+
+p5.RendererGL.prototype._getImmediateModeShader = function () {
+  if (this._defaultImmediateModeShader === undefined) {
+    this._defaultImmediateModeShader = new p5.Shader(this,
+      defaultShaders.immediateVert, defaultShaders.vertexColorFrag);
+  }
+  return this._defaultImmediateModeShader;
+};
+
+p5.RendererGL.prototype._getNormalShader = function () {
+  if (this._defaultNormalShader === undefined) {
+    this._defaultNormalShader = new p5.Shader(this,
+      defaultShaders.normalVert, defaultShaders.normalFrag);
+  }
+  return this._defaultNormalShader;
+};
+
+p5.RendererGL.prototype._getColorShader = function () {
+  if (this._defaultColorShader === undefined) {
+    this._defaultColorShader = new p5.Shader(this,
+      defaultShaders.normalVert, defaultShaders.basicFrag);
+  }
+  return this._defaultColorShader;
+};
+
+
+
+
 module.exports = p5.RendererGL;
